fd57fddd8f49175249b31b57cc524cb84cc0ea71
DavidHall-Code <david_m_hall@outlook.com>
2021-01-11 15:54:31 -0500

    Set up list in App.js and filled in Todo.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+//used example from webguided project index.js
+
 import React from 'react';
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
+import './components/Todo.css'
+
+const list = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 
 class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      todoList: list
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
