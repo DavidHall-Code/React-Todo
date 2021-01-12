ff39f4cbfc13caa10736faae61f721e17e2b40c7
DavidHall-Code <david_m_hall@outlook.com>
2021-01-11 19:18:55 -0500

    worked on App.js, completed TodoForm and TodoList

--- src/App.js
+++ src/App.js
@@ -27,6 +27,42 @@ class App extends React.Component {
       todoList: list
     }
   }
+
+  handleItemToggle = (itemId) => {
+    this.setState({
+      list: this.state.todoList.map(item => {
+        if (item.id === itemId) {
+          return {
+          ...item,
+          completed: !item.completed
+        }
+      }
+      return(item);
+    })
+  });
+  }
+  
+  handleItemAdd = (taskName) => {
+    const task = {
+      name: taskName,
+      id: Date.now(),
+      completed: false
+    }
+    this.setState({
+      todoList: [this.state.todoList, task]
+    })
+  }
+
+  handleItemCompleted = () => {
+    const newToDo = this.state.todoList.filter (item => {
+      return(!item.completed)
+    });
+    this.setState({
+      todoList: newToDo
+    })
+  }
+
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
