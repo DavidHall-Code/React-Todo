899530e645e4c95735ddad11bb53d12759715dc5
DavidHall-Code <david_m_hall@outlook.com>
2021-01-11 19:30:48 -0500

    finished setting up App.js running into issue

--- src/App.js
+++ src/App.js
@@ -68,8 +68,12 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <div className='App'>
+      <div className='header'>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm handleItemAdd={this.handleItemAdd} />
+      </div>
+        <TodoList todoList={this.state.todoList} handleItemCompleted={this.handleItemCompleted} handleItemToggle={this.handleItemtoggle} />
       </div>
     );
   }
